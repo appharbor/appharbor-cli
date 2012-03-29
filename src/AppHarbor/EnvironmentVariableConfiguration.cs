10facb5343bd9c81ceaf654762ff1e12d48e0807
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 00:53:35 -0700

    Add EnvironmentVariableConfiguration#Get

--- src/AppHarbor/EnvironmentVariableConfiguration.cs
+++ src/AppHarbor/EnvironmentVariableConfiguration.cs
@@ -4,6 +4,11 @@ namespace AppHarbor
 {
 	public class EnvironmentVariableConfiguration
 	{
+		public string Get(string variable, EnvironmentVariableTarget environmentVariableTarget)
+		{
+			return Environment.GetEnvironmentVariable(variable, environmentVariableTarget);
+		}
+
 		public void Set(string variable, string value, EnvironmentVariableTarget environmentVariableTarget)
 		{
 			Environment.SetEnvironmentVariable(variable, value, environmentVariableTarget);
