8329b673e5b5279914e38868d1144106bca66396
Rune Soerensen <runesoerensen@gmail.com>
2012-03-28 21:49:57 -0700

    Make AppHarborInstaller implement IWindsorInstaller

--- src/AppHarbor/AppHarborInstaller.cs
+++ src/AppHarbor/AppHarborInstaller.cs
@@ -1,6 +1,15 @@
-﻿namespace AppHarbor
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace AppHarbor
 {
-	public class AppHarborInstaller
+	public class AppHarborInstaller : IWindsorInstaller
 	{
+		public void Install(IWindsorContainer container, IConfigurationStore store)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
