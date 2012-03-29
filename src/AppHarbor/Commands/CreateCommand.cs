72079893f163242bdec38487396a94e625eb3af4
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 01:53:55 -0700

    Initialize CreateCommand with AppHarborApi

--- src/AppHarbor/Commands/CreateCommand.cs
+++ src/AppHarbor/Commands/CreateCommand.cs
@@ -4,6 +4,13 @@ namespace AppHarbor.Commands
 {
 	public class CreateCommand : ICommand
 	{
+		private readonly AppHarborApi _appHarborApi;
+
+		public CreateCommand(AppHarborApi appHarborApi)
+		{
+			_appHarborApi = appHarborApi;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
