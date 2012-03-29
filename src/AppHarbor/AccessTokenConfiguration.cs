6def353324ebb411d0fb1c44a5659cf82b372994
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 03:15:38 -0700

    Add AccessTokenConfiguration#DeleteAccessToken

--- src/AppHarbor/AccessTokenConfiguration.cs
+++ src/AppHarbor/AccessTokenConfiguration.cs
@@ -7,6 +7,11 @@ namespace AppHarbor
 		private const string TokenEnvironmentVariable = "AppHarborToken";
 		private const EnvironmentVariableTarget TokenEnvironmentVariableTarget = EnvironmentVariableTarget.User;
 
+		public virtual void DeleteAccessToken()
+		{
+			Environment.SetEnvironmentVariable(TokenEnvironmentVariable, null, TokenEnvironmentVariableTarget);
+		}
+
 		public virtual string GetAccessToken()
 		{
 			return Environment.GetEnvironmentVariable(TokenEnvironmentVariable, TokenEnvironmentVariableTarget);
