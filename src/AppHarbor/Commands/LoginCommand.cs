ca920cb950f0340d6ebd3a90c7a76f35961e213c
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 00:14:59 -0700

    Initialize LoginCommand with AppHarborApi

--- src/AppHarbor/Commands/LoginCommand.cs
+++ src/AppHarbor/Commands/LoginCommand.cs
@@ -4,6 +4,13 @@ namespace AppHarbor.Commands
 {
 	public class LoginCommand : ICommand
 	{
+		private readonly AppHarborApi _appHarborApi;
+
+		public LoginCommand(AppHarborApi appHarborApi)
+		{
+			_appHarborApi = appHarborApi;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
