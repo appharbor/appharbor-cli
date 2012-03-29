cc80d4e97294462063cd10cc54eb0d0f4a892d88
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 00:39:05 -0700

    Verify that LoginCommand#Execute asks for username and password

--- src/AppHarbor.Tests/Commands/LoginCommandTest.cs
+++ src/AppHarbor.Tests/Commands/LoginCommandTest.cs
@@ -1,6 +1,29 @@
-﻿namespace AppHarbor.Tests.Commands
+﻿using System;
+using System.IO;
+using AppHarbor.Commands;
+using Xunit;
+
+namespace AppHarbor.Tests.Commands
 {
 	public class LoginCommandTest
 	{
+		public void ShouldAskForUsernameAndPassword()
+		{
+			using (var writer = new StringWriter())
+			{
+				Console.SetOut(writer);
+
+				using (var reader = new StringReader(string.Format("foo{0}bar{0}", Environment.NewLine)))
+				{
+					Console.SetIn(reader);
+
+					var loginCommand = new LoginCommand(null);
+					loginCommand.Execute(new string[] { });
+
+					var expected = string.Format("Username:{0}Password:{0}", Environment.NewLine);
+					Assert.Equal(expected, writer.ToString());
+				}
+			}
+		}
 	}
 }
