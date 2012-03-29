e74f7b2647caeeb872fda91cb74a65a39c3becbc
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 00:59:13 -0700

    Throw an exception if user is already logged in

--- src/AppHarbor.Tests/Commands/LoginCommandTest.cs
+++ src/AppHarbor.Tests/Commands/LoginCommandTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using AppHarbor.Commands;
+using Moq;
 using Xunit;
 
 namespace AppHarbor.Tests.Commands
@@ -25,6 +26,19 @@ namespace AppHarbor.Tests.Commands
 					Assert.Equal(expected, writer.ToString());
 				}
 			}
+		}
+
+		[Fact]
+		public void ShouldThrowIfUserIsAlreadyLoggedIn()
+		{
+			var environmentVariableConfigurationMock = new Mock<EnvironmentVariableConfiguration>();
+			environmentVariableConfigurationMock.Setup(x => 
+				x.Get(It.IsAny<string>(), It.IsAny<EnvironmentVariableTarget>())).Returns("bar");
+
+			var loginCommand = new LoginCommand(null, environmentVariableConfigurationMock.Object);
+
+			var exception = Assert.Throws<CommandException>(() => loginCommand.Execute(new string[] { }));
+			Assert.Equal("You're already logged in", exception.Message);
 		}
 	}
 }
--- src/AppHarbor/Commands/LoginCommand.cs
+++ src/AppHarbor/Commands/LoginCommand.cs
@@ -15,6 +15,11 @@ namespace AppHarbor.Commands
 
 		public void Execute(string[] arguments)
 		{
+			if (_environmentVariableConfiguration.Get("AppHarborToken", EnvironmentVariableTarget.User) != null)
+			{
+				throw new CommandException("You're already logged in");
+			}
+
 			Console.WriteLine("Username:");
 			var username = Console.ReadLine();
 
