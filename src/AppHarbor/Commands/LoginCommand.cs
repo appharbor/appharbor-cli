1533d9ad8f18442759763e8b5a8a12667f79c090
Rune Soerensen <runesoerensen@gmail.com>
2012-03-29 00:13:38 -0700

    Make LoginCommand implement ICommand

--- src/AppHarbor/Commands/LoginCommand.cs
+++ src/AppHarbor/Commands/LoginCommand.cs
@@ -1,6 +1,12 @@
-﻿namespace AppHarbor.Commands
+﻿using System;
+
+namespace AppHarbor.Commands
 {
-	public class LoginCommand
+	public class LoginCommand : ICommand
 	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
