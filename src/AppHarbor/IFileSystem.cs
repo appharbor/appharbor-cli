3688dd3d56d43b42c29e0552e0016cdceb3fe32a
Rune Soerensen <runesoerensen@gmail.com>
2012-04-02 21:50:56 -0700

    Add required IFileSystem interface members

--- src/AppHarbor/IFileSystem.cs
+++ src/AppHarbor/IFileSystem.cs
@@ -1,6 +1,11 @@
-﻿namespace AppHarbor
+﻿using System.IO;
+
+namespace AppHarbor
 {
 	public interface IFileSystem
 	{
+		void Delete(string path);
+		Stream OpenRead(string path);
+		Stream OpenWrite(string path);
 	}
 }
