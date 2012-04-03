4344629ea8e36346264163c9da3217b55b9e0640
Rune Soerensen <runesoerensen@gmail.com>
2012-04-03 00:40:41 -0700

    Add ApplicationConfiguration#GetApplicationId

--- src/AppHarbor.Tests/ApplicationConfigurationTest.cs
+++ src/AppHarbor.Tests/ApplicationConfigurationTest.cs
@@ -1,6 +1,25 @@
-﻿namespace AppHarbor.Tests
+﻿using System.IO;
+using System.Text;
+using Moq;
+using Xunit;
+
+namespace AppHarbor.Tests
 {
 	public class ApplicationConfigurationTest
 	{
+		[Fact]
+		public void ShouldReturnApplicationIdIfConfigurationFileExists()
+		{
+			var fileSystem = new Mock<IFileSystem>();
+			var applicationName = "bar";
+
+			var configurationFile = Path.GetFullPath(".appharbor");
+			var stream = new MemoryStream(Encoding.Default.GetBytes(applicationName));
+
+			fileSystem.Setup(x => x.OpenRead(configurationFile)).Returns(stream);
+
+			var applicationConfiguration = new ApplicationConfiguration(fileSystem.Object);
+			Assert.Equal(applicationName, applicationConfiguration.GetApplicationId());
+		}
 	}
 }
--- src/AppHarbor/ApplicationConfiguration.cs
+++ src/AppHarbor/ApplicationConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace AppHarbor
+﻿using System.IO;
+
+namespace AppHarbor
 {
 	public class ApplicationConfiguration
 	{
@@ -7,6 +9,26 @@
 		public ApplicationConfiguration(IFileSystem fileSystem)
 		{
 			_fileSystem = fileSystem;
+		}
+
+		public string GetApplicationId()
+		{
+			var directory = Directory.GetCurrentDirectory();
+			var appharborConfigurationFile = new FileInfo(Path.Combine(directory, ".appharbor"));
+
+			try
+			{
+				var stream = _fileSystem.OpenRead(appharborConfigurationFile.FullName);
+				using (var reader = new StreamReader(stream))
+				{
+					return reader.ReadToEnd();
+				}
+
+			}
+			catch (FileNotFoundException)
+			{
+				throw new ApplicationConfigurationException("Application is not configured");
+			}
 		}
 	}
 }
