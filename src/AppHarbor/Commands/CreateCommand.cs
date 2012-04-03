8a335b67d38703c59d0e8661149eb9f3610fa753
Rune Soerensen <runesoerensen@gmail.com>
2012-04-02 22:16:26 -0700

    Initialize CreateCommand with an IFileSystem

--- src/AppHarbor/Commands/CreateCommand.cs
+++ src/AppHarbor/Commands/CreateCommand.cs
@@ -5,10 +5,12 @@ namespace AppHarbor.Commands
 	public class CreateCommand : ICommand
 	{
 		private readonly AppHarborClient _appHarborClient;
+		private readonly IFileSystem _fileSystem;
 
-		public CreateCommand(AppHarborClient appHarborClient)
+		public CreateCommand(AppHarborClient appHarborClient, IFileSystem fileSystem)
 		{
 			_appHarborClient = appHarborClient;
+			_fileSystem = fileSystem;
 		}
 
 		public void Execute(string[] arguments)
