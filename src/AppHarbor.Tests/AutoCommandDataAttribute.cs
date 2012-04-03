995492a73f4be5ae2f52438f843b39190ccdf2b0
Rune Soerensen <runesoerensen@gmail.com>
2012-04-03 03:30:01 -0700

    Initialize AutoCommandDataAttribute with an AutoMoqCustomization

--- src/AppHarbor.Tests/AutoCommandDataAttribute.cs
+++ src/AppHarbor.Tests/AutoCommandDataAttribute.cs
@@ -1,8 +1,14 @@
-﻿using Ploeh.AutoFixture.Xunit;
+﻿using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit;
 
 namespace AppHarbor.Tests
 {
 	public class AutoCommandDataAttribute : AutoDataAttribute
 	{
+		public AutoCommandDataAttribute()
+			: base(new Fixture().Customize(new AutoMoqCustomization()))
+		{
+		}
 	}
 }
