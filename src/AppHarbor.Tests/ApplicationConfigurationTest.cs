b24a0a434221adc3e5a67a095a698a4955e720d7
Rune Soerensen <runesoerensen@gmail.com>
2012-04-03 00:44:25 -0700

    Verify that an ApplicationConfigurationException is thrown if no configuration file exists

--- src/AppHarbor.Tests/ApplicationConfigurationTest.cs
+++ src/AppHarbor.Tests/ApplicationConfigurationTest.cs
@@ -7,19 +7,30 @@ namespace AppHarbor.Tests
 {
 	public class ApplicationConfigurationTest
 	{
+		public static string ConfigurationFile = Path.GetFullPath(".appharbor");
+
 		[Fact]
 		public void ShouldReturnApplicationIdIfConfigurationFileExists()
 		{
 			var fileSystem = new Mock<IFileSystem>();
 			var applicationName = "bar";
 
-			var configurationFile = Path.GetFullPath(".appharbor");
+			var configurationFile = ConfigurationFile;
 			var stream = new MemoryStream(Encoding.Default.GetBytes(applicationName));
 
 			fileSystem.Setup(x => x.OpenRead(configurationFile)).Returns(stream);
 
 			var applicationConfiguration = new ApplicationConfiguration(fileSystem.Object);
 			Assert.Equal(applicationName, applicationConfiguration.GetApplicationId());
+		}
+
+		[Fact]
+		public void ShouldThrowIfApplicationFileDoesNotExist()
+		{
+			var fileSystem = new InMemoryFileSystem();
+			var applicationConfiguration = new ApplicationConfiguration(fileSystem);
+
+			Assert.Throws<ApplicationConfigurationException>(() => applicationConfiguration.GetApplicationId());
 		}
 	}
 }
