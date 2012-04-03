1a298503a6d65b954405e28d014902e65b1ce5e8
Rune Soerensen <runesoerensen@gmail.com>
2012-04-03 15:23:35 -0700

    Initialize GitExecutable with the git executable path

--- src/AppHarbor/GitExecutable.cs
+++ src/AppHarbor/GitExecutable.cs
@@ -1,6 +1,17 @@
-﻿namespace AppHarbor
+﻿using System;
+using System.IO;
+
+namespace AppHarbor
 {
 	public class GitExecutable
 	{
+		private readonly FileInfo _gitExecutable;
+
+		public GitExecutable()
+		{
+			var programFilesPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+			var gitExecutablePath = Path.Combine(programFilesPath, "Git", "bin", "git.exe");
+			_gitExecutable = new FileInfo(gitExecutablePath);
+		}
 	}
 }
