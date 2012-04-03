0cc472f38940f7ad11a923907941fa31d0139bbd
Rune Soerensen <runesoerensen@gmail.com>
2012-04-03 03:43:08 -0700

    Remove unused constructor

--- src/AppHarbor.Tests/Commands/CreateCommandTest.cs
+++ src/AppHarbor.Tests/Commands/CreateCommandTest.cs
@@ -1,8 +1,6 @@
 ﻿using System.Linq;
 using AppHarbor.Commands;
 using Moq;
-using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.AutoMoq;
 using Ploeh.AutoFixture.Xunit;
 using Xunit.Extensions;
 
@@ -10,12 +8,6 @@ namespace AppHarbor.Tests.Commands
 {
 	public class CreateCommandTest
 	{
-		private readonly IFixture _fixture;
-
-		public CreateCommandTest()
-		{
-			_fixture = new Fixture().Customize(new AutoMoqCustomization());
-		}
 		[Theory, AutoCommandData]
 		public void ShouldCreateApplication([Frozen]Mock<IAppHarborClient> client, CreateCommand command, string[] arguments)
 		{
