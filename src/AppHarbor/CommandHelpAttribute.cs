be4e535e16958ffcbfa49132d06196d0451fcc82
Rune Soerensen <runesoerensen@gmail.com>
2012-04-04 02:57:17 -0700

    Add CommandHelpAttribute Description and Options

--- src/AppHarbor/CommandHelpAttribute.cs
+++ src/AppHarbor/CommandHelpAttribute.cs
@@ -4,5 +4,22 @@ namespace AppHarbor
 {
 	public class CommandHelpAttribute : Attribute
 	{
+		public CommandHelpAttribute(string description, string options = "")
+		{
+			Description = description;
+			Options = options;
+		}
+
+		public string Description
+		{
+			get;
+			private set;
+		}
+
+		public string Options
+		{
+			get;
+			private set;
+		}
 	}
 }
