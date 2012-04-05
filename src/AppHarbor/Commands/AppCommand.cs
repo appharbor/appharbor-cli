4470ff1fc8e841c0c14c30421bd7e4794a05f624
Rune Soerensen <runesoerensen@gmail.com>
2012-04-05 00:04:24 -0700

    Make AppCommand implement ICommand

--- src/AppHarbor/Commands/AppCommand.cs
+++ src/AppHarbor/Commands/AppCommand.cs
@@ -1,6 +1,12 @@
-﻿namespace AppHarbor.Commands
+﻿using System;
+
+namespace AppHarbor.Commands
 {
-	public class AppCommand
+	public class AppCommand : ICommand
 	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
