f44374be7307d0b79d233b68b84311cf2533de1a
Rune Soerensen <runesoerensen@gmail.com>
2012-04-05 04:32:18 -0700

    HelpCommand STASH

--- src/AppHarbor.Tests/Commands/HelpCommandTest.cs
+++ src/AppHarbor.Tests/Commands/HelpCommandTest.cs
@@ -1,6 +1,40 @@
-﻿namespace AppHarbor.Tests.Commands
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AppHarbor.Commands;
+using Xunit;
+
+namespace AppHarbor.Tests.Commands
 {
 	public class HelpCommandTest
 	{
+		private static Type FooCommandType = typeof(FooCommand);
+		private static Type FooBarCommandType = typeof(FooBarCommand);
+		private static Type BazQuxCommandType = typeof(BazQuxCommand);
+
+		[CommandHelp("Lorem Ipsum motherfucker", "[bar]")]
+		class FooCommand { }
+
+		[CommandHelp("Lorem Ipsum motherfucker", "[bar]")]
+		class FooBarCommand { }
+
+		[CommandHelp("Ipsum lol", "[Quz]")]
+		class BazQuxCommand { }
+
+		[Fact]
+		public void ShouldOutputHelpInformation()
+		{
+			var types = new List<Type> { FooCommandType, BazQuxCommandType, FooBarCommandType };
+			var helpCommand = new HelpCommand(types);
+
+			using (var writer = new StringWriter())
+			{
+				Console.SetOut(writer);
+
+				helpCommand.Execute(new string[0]);
+
+				Assert.Equal("Usage: appharbor COMMAND [command-options]\r\n\r\nAvailable commands:\r\n\r\n  qux:baz [quz]          #  Ipsum lol\r\n  bar:foo [bar]          #  Lorem Ipsum motherfucker\r\n  foo [bar]              #  Lorem Ipsum motherfucker\r\n", writer.ToString());
+			}
+		}
 	}
 }
--- src/AppHarbor/Commands/HelpCommand.cs
+++ src/AppHarbor/Commands/HelpCommand.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
 
 namespace AppHarbor.Commands
 {
@@ -15,7 +18,80 @@ namespace AppHarbor.Commands
 
 		public void Execute(string[] arguments)
 		{
-			throw new NotImplementedException();
+			Console.WriteLine("Usage: appharbor COMMAND [command-options]");
+			Console.WriteLine("");
+
+			Console.WriteLine("Available commands:");
+			Console.WriteLine("");
+
+			foreach (var commandType in _commandTypes.Where(x => x.IsClass)
+				.OrderBy(x => Reverse(x.Name))
+				.Reverse()
+				.OrderBy(x => x.Name.Length)
+				.OrderBy(x => x.Name))
+			{
+				var usageStringBuilder = new StringBuilder();
+
+				var splitted = SplitUpperCase(commandType.Name).Where(x => x != "Command");
+				usageStringBuilder.Append("  ");
+				usageStringBuilder.Append(string.Join(":", splitted.Reverse()));
+				var helpAttribute = commandType.GetCustomAttributes(true).OfType<CommandHelpAttribute>().Single();
+					usageStringBuilder.Append(string.Format(" {0}", helpAttribute.Options));
+
+				while (usageStringBuilder.Length < 25)
+				{
+					usageStringBuilder.Append(" ");
+				}
+
+				Console.Write(usageStringBuilder.ToString().ToLower());
+				Console.Write(string.Concat("#  ", helpAttribute.Description));
+				Console.WriteLine();
+			}
+		}
+
+		private static string Reverse(string sz)
+		{
+			if (string.IsNullOrEmpty(sz) || sz.Length == 1)
+			{
+				return sz;
+			}
+
+			var chars = sz.ToCharArray();
+			Array.Reverse(chars);
+
+			return new string(chars);
+		}
+
+		private static string[] SplitUpperCase(string source)
+		{
+			if (source == null)
+				return new string[] { }; //Return empty array.
+
+			if (source.Length == 0)
+				return new string[] { "" };
+
+			StringCollection words = new StringCollection();
+			int wordStartIndex = 0;
+
+			char[] letters = source.ToCharArray();
+			// Skip the first letter. we don't care what case it is.
+			for (int i = 1; i < letters.Length; i++)
+			{
+				if (char.IsUpper(letters[i]))
+				{
+					//Grab everything before the current index.
+					words.Add(new String(letters, wordStartIndex, i - wordStartIndex));
+					wordStartIndex = i;
+				}
+			}
+
+			//We need to have the last word.
+			words.Add(new String(letters, wordStartIndex, letters.Length - wordStartIndex));
+
+			//Copy to a string array.
+			string[] wordArray = new string[words.Count];
+			words.CopyTo(wordArray, 0);
+			return wordArray;
 		}
 	}
 }
