79d7bf8f81b2f5b9b11c15263f9b3f126eb395a8
Rune Soerensen <runesoerensen@gmail.com>
2012-04-05 01:48:45 -0700

    Initialize HelpCommand with a list of command types

--- src/AppHarbor/Commands/HelpCommand.cs
+++ src/AppHarbor/Commands/HelpCommand.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AppHarbor.Commands
 {
 	[CommandHelp("Display help summary or help for a specific command", "[COMMAND]")]
 	public class HelpCommand : ICommand
 	{
+		private readonly IEnumerable<Type> _commandTypes;
+
+		public HelpCommand(IEnumerable<Type> commandTypes)
+		{
+			_commandTypes = commandTypes;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
