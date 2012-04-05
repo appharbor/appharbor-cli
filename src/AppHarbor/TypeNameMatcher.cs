3d7dae1f70cfe46550a3e437513f2cfc714ed582
Rune Soerensen <runesoerensen@gmail.com>
2012-04-04 18:05:37 -0700

    Initialize TypeNameMatcher with a collection of candidate types

--- src/AppHarbor/TypeNameMatcher.cs
+++ src/AppHarbor/TypeNameMatcher.cs
@@ -1,6 +1,15 @@
-﻿namespace AppHarbor
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppHarbor
 {
 	public class TypeNameMatcher
 	{
+		private readonly IEnumerable<Type> _candidateTypes;
+
+		public TypeNameMatcher(IEnumerable<Type> candidateTypes)
+		{
+			_candidateTypes = candidateTypes;
+		}
 	}
 }
