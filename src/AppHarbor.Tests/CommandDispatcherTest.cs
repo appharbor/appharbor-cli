ee303fc38c2fae26345fe4f751d8c1d4af69291e
Rune Soerensen <runesoerensen@gmail.com>
2012-04-04 23:31:33 -0700

    Verify that executing single argument command works as expected

--- src/AppHarbor.Tests/CommandDispatcherTest.cs
+++ src/AppHarbor.Tests/CommandDispatcherTest.cs
@@ -1,4 +1,10 @@
-﻿namespace AppHarbor.Tests
+﻿using System.Linq;
+using Castle.MicroKernel;
+using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+
+namespace AppHarbor.Tests
 {
 	public class CommandDispatcherTest
 	{
@@ -7,6 +13,29 @@
 			public virtual void Execute(string[] arguments)
 			{
 			}
+		}
+
+		[Theory]
+		[InlineAutoCommandData("foo", "foo", null)]
+		[InlineAutoCommandData("foo:bar", "foo", "bar")]
+		public void ShouldDispatchCommandWithASingleParameter(
+			string argument,
+			string commandName,
+			string scope,
+			[Frozen]Mock<ITypeNameMatcher> typeNameMatcher,
+			[Frozen]Mock<IKernel> kernel,
+			Mock<FooCommand> command,
+			CommandDispatcher commandDispatcher)
+		{
+			var commandType = typeof(FooCommand);
+			typeNameMatcher.Setup(x => x.GetMatchedType(commandName, scope)).Returns(commandType);
+
+			kernel.Setup(x => x.Resolve(commandType)).Returns(command.Object);
+
+			var dispatchArguments = new string[] { argument };
+			commandDispatcher.Dispatch(dispatchArguments);
+
+			command.Verify(x => x.Execute(It.Is<string[]>(y => !y.Any())));
 		}
 	}
 }
