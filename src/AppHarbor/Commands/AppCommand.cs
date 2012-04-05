039592a9e146f2f21b6f81cb27a2d6bf4362ad74
Rune Soerensen <runesoerensen@gmail.com>
2012-04-05 00:05:27 -0700

    Initialize AppCommand with an IAppHarborClient

--- src/AppHarbor/Commands/AppCommand.cs
+++ src/AppHarbor/Commands/AppCommand.cs
@@ -4,6 +4,13 @@ namespace AppHarbor.Commands
 {
 	public class AppCommand : ICommand
 	{
+		private readonly IAppHarborClient _client;
+
+		public AppCommand(IAppHarborClient appharborClient)
+		{
+			_client = appharborClient;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
