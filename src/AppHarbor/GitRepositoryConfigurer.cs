ecd47d28f9f168e94704baf0775089c2f02e3a50
Rune Soerensen <runesoerensen@gmail.com>
2012-04-05 18:04:42 -0700

    Initialize GitRepositoryConfigurer with an IGitExecutor

--- src/AppHarbor/GitRepositoryConfigurer.cs
+++ src/AppHarbor/GitRepositoryConfigurer.cs
@@ -2,5 +2,11 @@
 {
 	public class GitRepositoryConfigurer
 	{
+		private readonly IGitExecutor _executor;
+
+		public GitRepositoryConfigurer(IGitExecutor executor)
+		{
+			_executor = executor;
+		}
 	}
 }
