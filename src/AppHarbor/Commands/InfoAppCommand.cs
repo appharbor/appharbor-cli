89661e683d64ab7adfc0289c32671571faa01094
Rune Soerensen <runesoerensen@gmail.com>
2012-04-06 00:30:36 -0700

    Derive InfoAppCommand from ICommand

--- src/AppHarbor/Commands/InfoAppCommand.cs
+++ src/AppHarbor/Commands/InfoAppCommand.cs
@@ -1,6 +1,12 @@
-﻿namespace AppHarbor.Commands
+﻿using System;
+
+namespace AppHarbor.Commands
 {
-	public class InfoAppCommand
+	public class InfoAppCommand : ICommand
 	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
