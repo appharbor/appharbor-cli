32ce1a45e25a3e4cc836926bf3c73b1f774bde44
Rune Soerensen <runesoerensen@gmail.com>
2012-04-09 15:25:46 -0700

    Initialize AliasMatcher with a list of candidate types

--- src/AppHarbor/AliasMatcher.cs
+++ src/AppHarbor/AliasMatcher.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AppHarbor
 {
 	public class AliasMatcher : IAliasMatcher
 	{
+		private readonly IEnumerable<Type> _candidateTypes;
+
+		public AliasMatcher(IEnumerable<Type> candidateTypes)
+		{
+			_candidateTypes = candidateTypes;
+		}
+
 		public Type GetMatchedType(string commandArgument)
 		{
 			throw new NotImplementedException();
