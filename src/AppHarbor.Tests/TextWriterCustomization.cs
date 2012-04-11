5545674c50f649442d75323fc41910514862cb47
Rune Soerensen <runesoerensen@gmail.com>
2012-04-10 21:30:33 -0700

    Make TextWriterCustomization implement ICustomization

--- src/AppHarbor.Tests/TextWriterCustomization.cs
+++ src/AppHarbor.Tests/TextWriterCustomization.cs
@@ -1,6 +1,13 @@
-﻿namespace AppHarbor.Tests
+﻿using System;
+using Ploeh.AutoFixture
+
+namespace AppHarbor.Tests
 {
-	public class TextWriterCustomization
+	public class TextWriterCustomization : ICustomization
 	{
+		public void Customize(IFixture fixture)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
