e844330c3c199163a09908612da5f37a07373ae2
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 03:18:28 -0700

    Add default LinkCommand constructor

--- src/AppHarbor/Commands/LinkCommand.cs
+++ src/AppHarbor/Commands/LinkCommand.cs
@@ -4,6 +4,10 @@ namespace AppHarbor.Commands
 {
 	public class LinkCommand : ICommand
 	{
+		public LinkCommand()
+		{
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
