fecb4e7314a4ea473d6aab0b32af3112e61b14b8
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 02:38:24 -0700

    Add default AppDeleteCommand initializer

--- src/AppHarbor/Commands/AppDeleteCommand.cs
+++ src/AppHarbor/Commands/AppDeleteCommand.cs
@@ -5,6 +5,10 @@ namespace AppHarbor.Commands
 	[CommandHelp("Delete application")]
 	public class AppDeleteCommand : ICommand
 	{
+		public AppDeleteCommand()
+		{
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
