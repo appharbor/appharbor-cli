f7235deb5f87a2416a876595f415b59fcbd9069d
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 01:53:21 -0700

    Add MaskedConsoleInput#Get class stub

--- src/AppHarbor/MaskedConsoleInput.cs
+++ src/AppHarbor/MaskedConsoleInput.cs
@@ -1,6 +1,32 @@
-﻿namespace AppHarbor
+﻿using System;
+
+namespace AppHarbor
 {
 	public class MaskedConsoleInput
 	{
+		public string Get()
+		{
+			string input = "";
+			ConsoleKeyInfo key;
+
+			do
+			{
+				key = Console.ReadKey(true);
+
+				if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+				{
+					input += key.KeyChar;
+					Console.Write("*");
+				}
+				else if (key.Key == ConsoleKey.Backspace && input.Length > 0)
+				{
+					input = input.Substring(0, (input.Length - 1));
+					Console.Write("\b \b");
+				}
+			}
+			while (key.Key != ConsoleKey.Enter);
+
+			return input;
+		}
 	}
 }
