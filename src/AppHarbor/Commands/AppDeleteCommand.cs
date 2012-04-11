a1bfaeca53fa2ffd48084449487988f16022a224
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 02:33:13 -0700

    Make AppDeleteCommand implement ICommand

--- src/AppHarbor/Commands/AppDeleteCommand.cs
+++ src/AppHarbor/Commands/AppDeleteCommand.cs
@@ -1,6 +1,12 @@
-﻿namespace AppHarbor.Commands
+﻿using System;
+
+namespace AppHarbor.Commands
 {
-	public class AppDeleteCommand
+	public class AppDeleteCommand : ICommand
 	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
