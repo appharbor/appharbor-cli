a93eb5c17cce7d4e0a6e711b71d003382d87aa91
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 03:23:29 -0700

    Initialize LinkCommand with an IAppHArborClient

--- src/AppHarbor/Commands/LinkCommand.cs
+++ src/AppHarbor/Commands/LinkCommand.cs
@@ -5,10 +5,12 @@ namespace AppHarbor.Commands
 	public class LinkCommand : ICommand
 	{
 		private readonly IApplicationConfiguration _applicationConfiguration;
+		private readonly IAppHarborClient _appharborClient;
 
-		public LinkCommand(IApplicationConfiguration applicationConfiguration)
+		public LinkCommand(IApplicationConfiguration applicationConfiguration, IAppHarborClient appharborClient)
 		{
 			_applicationConfiguration = applicationConfiguration;
+			_appharborClient = appharborClient;
 		}
 
 		public void Execute(string[] arguments)
