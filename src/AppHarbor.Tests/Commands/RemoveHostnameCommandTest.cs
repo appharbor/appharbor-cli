57b4953123f8fd8f9e1393b93a1133d7ed3c2c1e
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 21:38:08 -0700

    Add spec for RemoveHostnameCommand

--- src/AppHarbor.Tests/Commands/RemoveHostnameCommandTest.cs
+++ src/AppHarbor.Tests/Commands/RemoveHostnameCommandTest.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AppHarbor.Commands;
+using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
 
 namespace AppHarbor.Tests.Commands
 {
 	public class RemoveHostnameCommandTest
 	{
+		[Theory, AutoCommandData]
+		public void ShouldThrowIfNoArguments(RemoveHostnameCommand command)
+		{
+			Assert.Throws<CommandException>(() => command.Execute(new string[0]));
+		}
+
+		[Theory]
+		[InlineAutoCommandData("example.com")]
+		public void ShouldRemoveHostname(string hostname,
+			[Frozen]Mock<IApplicationConfiguration> applicationConfiguration,
+			[Frozen]Mock<IAppHarborClient> client,
+			RemoveHostnameCommand command, string applicationId)
+		{
+			applicationConfiguration.Setup(x => x.GetApplicationId()).Returns(applicationId);
+
+			command.Execute(new string[] { hostname });
+
+			client.Verify(x => x.RemoveHostname(applicationId, hostname));
+		}
 	}
 }
