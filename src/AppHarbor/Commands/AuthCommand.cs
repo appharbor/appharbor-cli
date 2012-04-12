4a904509a742435c636893ff5fc92ff863214daa
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 20:45:49 -0700

    Initialize AuthCommand with an IAppHarborClient

--- src/AppHarbor/Commands/AuthCommand.cs
+++ src/AppHarbor/Commands/AuthCommand.cs
@@ -4,6 +4,13 @@ namespace AppHarbor.Commands
 {
 	public class AuthCommand : ICommand
 	{
+		private readonly IAppHarborClient _appharborClient;
+
+		public AuthCommand(IAppHarborClient appharborClient)
+		{
+			_appharborClient = appharborClient;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
