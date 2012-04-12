26e4a72ce4a6176d787532003b28190552c397cb
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 21:18:57 -0700

    Add AddHostnameCommand stubs

---
+++ src/AppHarbor/Commands/AddHostnameCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AppHarbor.Commands
+{
+	public class AddHostnameCommand : ICommand
+	{
+		private readonly IApplicationConfiguration _applicationConfiguration;
+		private readonly IAppHarborClient _appharborClient;
+
+		public AddHostnameCommand(IApplicationConfiguration applicationConfiguration, IAppHarborClient appharborClient)
+		{
+			_applicationConfiguration = applicationConfiguration;
+			_appharborClient = appharborClient;
+		}
+
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
