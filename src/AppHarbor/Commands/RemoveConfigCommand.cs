c16d2bd2532683f976cf475f6575825c55934728
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 20:00:34 -0700

    Initialize RemoveConfigCommand with an IApplicationConfiguration and IAppHarborClient

--- src/AppHarbor/Commands/RemoveConfigCommand.cs
+++ src/AppHarbor/Commands/RemoveConfigCommand.cs
@@ -4,6 +4,15 @@ namespace AppHarbor.Commands
 {
 	class RemoveConfigCommand : ICommand
 	{
+		private readonly IApplicationConfiguration _applicationConfiguration;
+		private readonly IAppHarborClient _appharborClient;
+
+		public RemoveConfigCommand(IApplicationConfiguration applicationConfiguration, IAppHarborClient appharborClient)
+		{
+			_applicationConfiguration = applicationConfiguration;
+			_appharborClient = appharborClient;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
