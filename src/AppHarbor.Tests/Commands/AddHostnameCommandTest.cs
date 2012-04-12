59a5f1e8eb4b5ede361d3d6c000a5eb41b0c2b71
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 21:24:20 -0700

    Add specs for AddHostnameCommand

--- src/AppHarbor.Tests/Commands/AddHostnameCommandTest.cs
+++ src/AppHarbor.Tests/Commands/AddHostnameCommandTest.cs
@@ -1,6 +1,31 @@
-﻿namespace AppHarbor.Tests.Commands
+﻿using AppHarbor.Commands;
+using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace AppHarbor.Tests.Commands
 {
 	public class AddHostnameCommandTest
 	{
+		[Theory, AutoCommandData]
+		public void ShouldThrowIfNoArguments(AddHostnameCommand command)
+		{
+			Assert.Throws<CommandException>(() => command.Execute(new string[0]));
+		}
+
+		[Theory]
+		[InlineAutoCommandData("example.com")]
+		public void ShouldAddConfigurationVariables(string hostname,
+			[Frozen]Mock<IApplicationConfiguration> applicationConfiguration,
+			[Frozen]Mock<IAppHarborClient> client,
+			AddHostnameCommand command, string applicationId)
+		{
+			applicationConfiguration.Setup(x => x.GetApplicationId()).Returns(applicationId);
+
+			command.Execute(new string[] { hostname });
+
+			client.Verify(x => x.CreateHostname(applicationId, hostname));
+		}
 	}
 }
