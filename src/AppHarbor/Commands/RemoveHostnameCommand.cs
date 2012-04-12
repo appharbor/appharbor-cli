1f2d8c3331dbe687a11229d1fcb07ed59a7b9eb2
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 21:38:35 -0700

    Initialize RemoveHostnameCommand with an IApplicationConfiguration and IAppHarborClient

--- src/AppHarbor/Commands/RemoveHostnameCommand.cs
+++ src/AppHarbor/Commands/RemoveHostnameCommand.cs
@@ -4,6 +4,15 @@ namespace AppHarbor.Commands
 {
 	public class RemoveHostnameCommand : ICommand
 	{
+		private readonly IApplicationConfiguration _applicationConfiguration;
+		private readonly IAppHarborClient _appharborClient;
+
+		public RemoveHostnameCommand(IApplicationConfiguration applicationConfiguration, IAppHarborClient appharborClient)
+		{
+			_applicationConfiguration = applicationConfiguration;
+			_appharborClient = appharborClient;
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
