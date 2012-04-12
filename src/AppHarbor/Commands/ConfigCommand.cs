658f92de9b535753be4c41747bcbd5b99a7222b9
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 20:19:05 -0700

    Add ConfigCommand stubs

---
+++ src/AppHarbor/Commands/ConfigCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppHarbor.Commands
+{
+	public class ConfigCommand : ICommand
+	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
