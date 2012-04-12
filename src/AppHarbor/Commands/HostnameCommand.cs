12b8814ec4d44bb82069d48bb328cba92e601912
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 21:01:17 -0700

    Add HostnameCommand stub

---
+++ src/AppHarbor/Commands/HostnameCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppHarbor.Commands
+{
+	public class HostnameCommand : ICommand
+	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
