14b7245def2a844961bfd537b26dbfd41c90e6d7
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 19:52:16 -0700

    Add RemoveConfigCommand stub

---
+++ src/AppHarbor/Commands/RemoveConfigCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppHarbor.Commands
+{
+	class RemoveConfigCommand : ICommand
+	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
