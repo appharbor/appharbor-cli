cc4f9b2f7b2462a9dc3e095cacdea67aaf3bfa93
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 23:33:58 -0700

    Add BuildCommand stubs

---
+++ src/AppHarbor/Commands/BuildCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppHarbor.Commands
+{
+	public class BuildCommand : ICommand
+	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
