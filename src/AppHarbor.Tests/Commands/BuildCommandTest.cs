b5a58339ee1ef273423fdeacf0cfb43ff149182f
Rune Soerensen <runesoerensen@gmail.com>
2012-04-12 01:44:48 -0700

    Implement BuildCommand#Execute

--- src/AppHarbor.Tests/Commands/BuildCommandTest.cs
+++ src/AppHarbor.Tests/Commands/BuildCommandTest.cs
@@ -1,6 +1,41 @@
-﻿namespace AppHarbor.Tests.Commands
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AppHarbor.Commands;
+using AppHarbor.Model;
+using Moq;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+using Xunit;
+
+namespace AppHarbor.Tests.Commands
 {
 	public class BuildCommandTest
 	{
+		[Theory, AutoCommandData]
+		public void ShouldOutputBuilds([Frozen]Mock<IApplicationConfiguration> applicationConfiguration,
+			[Frozen]Mock<IAppHarborClient> client,
+			[Frozen]Mock<TextWriter> writer,
+			BuildCommand command, string applicationId)
+		{
+			applicationConfiguration.Setup(x => x.GetApplicationId()).Returns(applicationId);
+			var builds = new List<Build>
+			{
+				new Build { Commit = new Build.CommitClass { Message = "foo bar", ID = "baz" }, Status = "Failed", Deployed = DateTime.Now },
+			};
+
+			client.Setup(x => x.GetBuilds(applicationId)).Returns(builds);
+
+			Assert.DoesNotThrow(() => command.Execute(new string[0]));
+		}
+
+		[Theory, AutoCommandData]
+		public void ShouldWriteIfNoBuildsExists(BuildCommand command,
+			[Frozen]Mock<TextWriter> writer, string applicationId)
+		{
+			command.Execute(new string[0]);
+
+			writer.Verify(x => x.WriteLine("No builds are associated with this application."));
+		}
 	}
 }
--- src/AppHarbor/Commands/BuildCommand.cs
+++ src/AppHarbor/Commands/BuildCommand.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace AppHarbor.Commands
 {
+	[CommandHelp("List latest builds")]
 	public class BuildCommand : ICommand
 	{
+		public const string OutputFormat = "{0,-43}{1,-13}{2,5}";
+
 		private readonly IApplicationConfiguration _applicationConfiguration;
 		private readonly IAppHarborClient _appharborClient;
 		private readonly TextWriter _writer;
@@ -18,7 +22,24 @@ namespace AppHarbor.Commands
 
 		public void Execute(string[] arguments)
 		{
-			throw new NotImplementedException();
+			var applicationId = _applicationConfiguration.GetApplicationId();
+			var builds = _appharborClient.GetBuilds(applicationId);
+
+			_writer.WriteLine(string.Format(OutputFormat, "Commit", "Status", "Deployed"));
+			foreach (var build in builds)
+			{
+				var commitId = GetShortened(build.Commit.ID, 7);
+
+				var message = GetShortened(string.Concat(commitId, " | ", build.Commit.Message.Trim('\n')), 40, "...");
+				var buildOutput = String.Format(OutputFormat, message, build.Status, build.Deployed);
+
+				_writer.WriteLine(buildOutput);
+			}
+		}
+
+		private static string GetShortened(string input, int width, string continuedString = "")
+		{
+			return input.Length > width ? input.Substring(0, width - continuedString.Length) + continuedString : input;
 		}
 	}
 }
