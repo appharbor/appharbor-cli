f118cae2a0dde164a5378c12b41db06347f98bb4
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 19:27:55 -0700

    Initialize AppConfigCommand with an IAppHarborClient

--- src/AppHarbor/Commands/AddConfigCommand.cs
+++ src/AppHarbor/Commands/AddConfigCommand.cs
@@ -5,10 +5,12 @@ namespace AppHarbor.Commands
 	public class AddConfigCommand : ICommand
 	{
 		private readonly IApplicationConfiguration _applicationConfiguration;
+		private readonly IAppHarborClient _appharborClient;
 
-		public AddConfigCommand(IApplicationConfiguration applicationConfiguration)
+		public AddConfigCommand(IApplicationConfiguration applicationConfiguration, IAppHarborClient appharborClient)
 		{
 			_applicationConfiguration = applicationConfiguration;
+			_appharborClient = appharborClient;
 		}
 
 		public void Execute(string[] arguments)
