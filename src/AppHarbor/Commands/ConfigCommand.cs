1168e7d41129633ff4497199c7752b6e7cfdbdcf
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 20:26:42 -0700

    Initialize ConfigCommand with an ApplicationConfiguration, AppHarborClient and TextWriter

--- src/AppHarbor/Commands/ConfigCommand.cs
+++ src/AppHarbor/Commands/ConfigCommand.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.IO;
 
 namespace AppHarbor.Commands
 {
 	public class ConfigCommand : ICommand
 	{
+		private readonly IApplicationConfiguration _applicationConfiguration;
+		private readonly IAppHarborClient _appharborClient;
+		private readonly TextWriter _writer;
+
+		public ConfigCommand(IApplicationConfiguration applicationConfiguration, IAppHarborClient appharborClient, TextWriter writer)
+		{
+			_applicationConfiguration = applicationConfiguration;
+			_appharborClient = appharborClient;
+			_writer = writer
+		}
+
 		public void Execute(string[] arguments)
 		{
 			throw new NotImplementedException();
