0fd092283c9e055d514153997eecc3ed4e16d849
Rune Soerensen <runesoerensen@gmail.com>
2012-04-11 20:41:08 -0700

    Add AuthCommand stubs

---
+++ src/AppHarbor/Commands/AuthCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppHarbor.Commands
+{
+	public class AuthCommand : ICommand
+	{
+		public void Execute(string[] arguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
