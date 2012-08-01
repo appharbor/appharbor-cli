100f81b26a7ca27fcd57cade643a38ef4d20829d
Rune Soerensen <runesoerensen@gmail.com>
2012-07-31 20:31:04 -0700

    Add compression extensions

---
+++ src/AppHarbor/CompressionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using ICSharpCode.SharpZipLib.Tar;
+
+namespace AppHarbor
+{
+	public static class CompressionExtensions
+	{
+		public static void ToTar(this DirectoryInfo sourceDirectory, Stream output)
+		{
+			var archive = TarArchive.CreateOutputTarArchive(output);
+
+			archive.RootPath = sourceDirectory.FullName.Replace(Path.DirectorySeparatorChar, '/').TrimEnd('/');
+
+			var entries =
+				from x in sourceDirectory.GetFiles("*", SearchOption.AllDirectories)
+				select TarEntry.CreateEntryFromFile(x.FullName);
+
+			foreach (var entry in entries)
+			{
+				archive.WriteEntry(entry, true);
+			}
+
+			archive.Close();
+		}
+	}
+}
