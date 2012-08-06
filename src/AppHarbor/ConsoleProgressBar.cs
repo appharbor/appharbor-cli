9ed76fef033d459abf46e1bc23da3b0521fb2ded
Rune Soerensen <runesoerensen@gmail.com>
2012-08-06 14:29:05 -0700

    Initialize ConsoleProgressBar with a writer

--- src/AppHarbor/ConsoleProgressBar.cs
+++ src/AppHarbor/ConsoleProgressBar.cs
@@ -1,6 +1,14 @@
-﻿namespace AppHarbor
+﻿using System.IO;
+
+namespace AppHarbor
 {
 	public class ConsoleProgressBar
 	{
+		private readonly TextWriter _writer;
+
+		public ConsoleProgressBar(TextWriter writer)
+		{
+			_writer = writer;
+		}
 	}
 }
