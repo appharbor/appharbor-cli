dfe01dcdda4b38f10e9cce9eaf8629287f142ce6
Rune Soerensen <runesoerensen@gmail.com>
2012-08-07 19:36:25 -0700

    Add FederatedAmazonCredentials stub

---
+++ src/AppHarbor/FederatedAmazonCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace AppHarbor
+{
+	public class FederatedAmazonCredentials
+	{
+		public string AccessKeyId { get; set; }
+		public string SecretAccessKey { get; set; }
+		public string SessionToken { get; set; }
+	}
+}
