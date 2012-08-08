4a9b59d22c4869b32784bd134a26f367aa80bd64
Rune Soerensen <runesoerensen@gmail.com>
2012-08-07 19:39:40 -0700

    Add FederatedAmazonCredentials#GetSessionCredentials

--- src/AppHarbor/FederatedAmazonCredentials.cs
+++ src/AppHarbor/FederatedAmazonCredentials.cs
@@ -1,9 +1,16 @@
-﻿namespace AppHarbor
+﻿using Amazon.Runtime;
+
+namespace AppHarbor
 {
 	public class FederatedAmazonCredentials
 	{
 		public string AccessKeyId { get; set; }
 		public string SecretAccessKey { get; set; }
 		public string SessionToken { get; set; }
+
+		public SessionAWSCredentials GetSessionCredentials()
+		{
+			return new SessionAWSCredentials(AccessKeyId, SecretAccessKey, SessionToken);
+		}
 	}
 }
