ca02c81a9814eece7c31cdd3ad6b7d2d3bd1a5d8
Rune Soerensen <runesoerensen@gmail.com>
2012-08-08 19:28:57 -0700

    Remove test code

--- src/AppHarbor/Commands/InfoAppCommand.cs
+++ src/AppHarbor/Commands/InfoAppCommand.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using NDesk.Options;
+﻿using System.IO;
 
 namespace AppHarbor.Commands
 {
@@ -16,28 +14,7 @@ namespace AppHarbor.Commands
 			_client = client;
 			_applicationConfiguration = applicationConfiguration;
 			_writer = writer;
-
-			Options = new OptionSet()
-            {
-                {"b|booleanOption", "Boolean flag option", b => BooleanOption = true},
-                {"l|list=", "Values to add to list", v => OptionalArgumentList.Add(v)},
-                {"r|requiredArguments=", "Optional string argument requiring a value be specified afterwards", s => OptionalArgument1 = s},
-                {"o|optionalArgument:", "Optional String argument which is null if no value follow is specified", s => OptionalArgument2 = s ?? "<no argument specified>"}
-            };
-
-			this.HasRequiredOption("requiredOption=", "Required string argument also requiring a value.", s => { });
-			this.HasOption("anotherOptional=", "Another way to specify optional arguments", s => { });
-
-			HasAdditionalArguments(2, "<Argument1> <Argument2>");
 		}
-
-
-		public string Argument1;
-		public string Argument2;
-		public string OptionalArgument1;
-		public string OptionalArgument2;
-		public bool BooleanOption;
-		public List<string> OptionalArgumentList = new List<string>();
 
 		public override void Run(string[] arguments)
 		{
