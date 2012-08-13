35c0f96e8960b95b460f6e38ab858109b9d47230
Rune Soerensen <runesoerensen@gmail.com>
2012-08-12 19:16:52 -0700

    Initialize ApplicationCommand with an ApplicationConfiguration

--- src/AppHarbor/Commands/ApplicationCommand.cs
+++ src/AppHarbor/Commands/ApplicationCommand.cs
@@ -2,5 +2,11 @@
 {
 	public abstract class ApplicationCommand : Command
 	{
+		private readonly IApplicationConfiguration _applicationConfiguration;
+
+		public ApplicationCommand(IApplicationConfiguration applicationConfiguration)
+		{
+			_applicationConfiguration = applicationConfiguration;
+		}
 	}
 }
