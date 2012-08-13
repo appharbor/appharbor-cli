ce82baf303de633728c023e94fb478215167645d
Rune Soerensen <runesoerensen@gmail.com>
2012-08-12 19:31:21 -0700

    Add and parse application id if specified

--- src/AppHarbor/Commands/ApplicationCommand.cs
+++ src/AppHarbor/Commands/ApplicationCommand.cs
@@ -3,17 +3,19 @@
 	public abstract class ApplicationCommand : Command
 	{
 		private readonly IApplicationConfiguration _applicationConfiguration;
+		private string _applicationId;
 
 		public ApplicationCommand(IApplicationConfiguration applicationConfiguration)
 		{
 			_applicationConfiguration = applicationConfiguration;
+			OptionSet.Add("a|application=", "Optional application id", x => _applicationId = x);
 		}
 
 		protected string ApplicationId
 		{
 			get
 			{
-				return _applicationConfiguration.GetApplicationId();
+				return _applicationId ?? _applicationConfiguration.GetApplicationId();
 			}
 		}
 	}
