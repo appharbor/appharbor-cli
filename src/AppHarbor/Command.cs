f304727467eefe01ac13783c11ab27448dee1959
Rune Soerensen <runesoerensen@gmail.com>
2012-08-12 17:40:37 -0700

    Add Command#OptionSet

--- src/AppHarbor/Command.cs
+++ src/AppHarbor/Command.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using NDesk.Options;
 
 namespace AppHarbor
 {
 	public abstract class Command
 	{
+		private readonly OptionSet _optionSet;
+
+		public Command()
+		{
+			_optionSet = new OptionSet();
+		}
+
 		public abstract void Execute(string[] arguments);
+
+		public OptionSet OptionSet
+		{
+			get
+			{
+				return _optionSet;
+			}
+		}
 	}
 }
