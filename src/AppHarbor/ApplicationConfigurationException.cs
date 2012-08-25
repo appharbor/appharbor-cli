836933a7c2362fb04e5dd6444db7c23ededdcffd
Michael Friis <friism@gmail.com>
2012-08-24 17:48:33 -0700

    remove and sort usings

--- src/AppHarbor/ApplicationConfigurationException.cs
+++ src/AppHarbor/ApplicationConfigurationException.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace AppHarbor
+﻿namespace AppHarbor
 {
 	public class ApplicationConfigurationException : CommandException
 	{
--- src/AppHarbor/IAccessTokenConfiguration.cs
+++ src/AppHarbor/IAccessTokenConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace AppHarbor
+﻿namespace AppHarbor
 {
 	public interface IAccessTokenConfiguration
 	{
