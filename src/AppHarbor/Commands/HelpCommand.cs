2710d864b0c87a4d1c3b629f19006e52d7f21641
Rune Soerensen <rs@appharbor.com>
2014-11-15 18:55:33 -0800

    Remove comments

--- src/AppHarbor/Commands/HelpCommand.cs
+++ src/AppHarbor/Commands/HelpCommand.cs
@@ -86,7 +86,7 @@ namespace AppHarbor.Commands
 		private static string[] SplitUpperCase(string source)
 		{
 			if (source == null)
-				return new string[] { }; //Return empty array.
+				return new string[] { };
 
 			if (source.Length == 0)
 				return new string[] { "" };
@@ -95,21 +95,18 @@ namespace AppHarbor.Commands
 			int wordStartIndex = 0;
 
 			char[] letters = source.ToCharArray();
-			// Skip the first letter. we don't care what case it is.
+
 			for (int i = 1; i < letters.Length; i++)
 			{
 				if (char.IsUpper(letters[i]))
 				{
-					//Grab everything before the current index.
 					words.Add(new String(letters, wordStartIndex, i - wordStartIndex));
 					wordStartIndex = i;
 				}
 			}
 
-			//We need to have the last word.
 			words.Add(new String(letters, wordStartIndex, letters.Length - wordStartIndex));
 
-			//Copy to a string array.
 			string[] wordArray = new string[words.Count];
 			words.CopyTo(wordArray, 0);
 			return wordArray;
